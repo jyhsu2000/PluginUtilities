f5ad6e04ff41600372e3bd965079ce243802245e
KID <jyhsu2000@yahoo.com.tw>
2017-08-23 17:17:32 +0800

    Attach source and javadoc

--- src/main/java/club/kid7/pluginutilities/kitemstack/KItemStack.java
+++ src/main/java/club/kid7/pluginutilities/kitemstack/KItemStack.java
@@ -17,7 +17,7 @@ import java.util.List;
  * This is a chainable ItemStack for {@link ItemStack}s in {@link Bukkit}
  * <br>
  * Example Usage:<br>
- * {@code ItemStack is = new KItemStack(Material.LEATHER_HELMET).amount(2).data(4).durability(4).enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.LUCK, 2).name(ChatColor.RED + "the name").lore(ChatColor.GREEN + "line 1").lore(ChatColor.BLUE + "line 2").color(Color.MAROON);
+ * {@code ItemStack is = new KItemStack(Material.LEATHER_HELMET).amount(2).data(4).durability(4).enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.LUCK, 2).name(ChatColor.RED + "the name").lore(ChatColor.GREEN + "line 1").lore(ChatColor.BLUE + "line 2").color(Color.MAROON);}
  *
  * @author MiniDigger, jyhsu(KID)
  * @version 1.0
